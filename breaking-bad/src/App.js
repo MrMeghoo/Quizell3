5d7a88f3bf8d696f5211654961e75d200d4921db
MrMeghoo <mikelmeghoo@gmail.com>
2024-06-20 21:39:33 -0400

    react solo project

---
+++ breaking-bad/src/App.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Navbar"
+import SearchBar from "./components/Searchbar"
+import CharacterList from "./components/Characterlist"
+import axios from "./components/api";
+import { Spinner } from "react-bootstrap";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const results = await axios.get(`/characters?name=${query}`);
+      setItems(results.data);
+      setLoading(false);
+      console.log({ results });
+    };
+    fetchItems();
+  }, [query]);
+  return (
+    <div className="app">
+      <Header />
+      <SearchBar setQuery={(query) => setQuery(query)} />
+      {loading ? <Spinner /> : <CharacterList items={items} />}
+    </div>
+  );
+}
+
+
+export default App;
